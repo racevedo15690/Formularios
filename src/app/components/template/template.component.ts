4f600194e3eae88d8f34044b1d0e40feaa5066c8
Ricardo Acevedo <robertoacevedo2000@hotmail.com>
2019-05-17 09:49:45 -0600

    segundo

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -1,17 +1,50 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { pairs } from 'rxjs';
 
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
-  styles: []
+  styles: [`
+    .ng-invalid.ng-touched:not(form) {
+      border: 1px solid red;
+    }
+  `]
 })
 export class TemplateComponent {
+
+  usuario: Object = {
+    nombre: null,
+    apellido: null,
+    correo: null,
+    pais: '',
+    sexo: '',
+    acepta: false
+  };
+
+  paises = [{
+      codigo: 'CRI',
+      nombre: 'Costa Rica'
+    },
+    {
+      codigo: 'GT',
+      nombre: 'Guatemala'
+    },
+    {
+      codigo: 'ESP',
+      nombre: 'España'
+    }
+  ];
+
+  sexos: string[] = ['Hombre', 'Mujer', 'Indefinido'];
 
   constructor() { }
 
 
-  guardar() {
-    console.log('formulario posteadeo');
+  guardar( forma: NgForm ) {
+    console.log( 'NgForm', forma );
+    console.log( 'Valor Forma', forma.value );
+    console.log('Usuario', this.usuario);
   }
 
 }
