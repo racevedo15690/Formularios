cdda69c1ae7c5ea05fa8470cdbed9a6dbdf82134
Ricardo Acevedo <robertoacevedo2000@hotmail.com>
2019-05-17 15:15:49 -0600

    tercero

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
   styles: []
 })
-export class DataComponent implements OnInit {
+export class DataComponent  {
 
-  constructor() { }
+  forma: FormGroup;
 
-  ngOnInit() {
+  constructor() {
+    this.forma = new FormGroup({
+      'nombre': new FormControl('', [Validators.required, Validators.minLength(3)]),
+      'apellido': new FormControl('', Validators.required),
+      'correo': new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')])
+    });
+  }
+
+  guardarCambios(){
+    console.log(this.forma.value);
   }
 
 }
